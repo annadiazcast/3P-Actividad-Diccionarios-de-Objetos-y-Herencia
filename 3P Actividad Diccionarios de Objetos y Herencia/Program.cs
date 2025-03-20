4b9e12181af8b80700e2f176c6581d935a8193e9
Lab2-23 <Lab2-23@LAB2-23>
2025-03-20 17:38:19 -0600

    Agregar archivos de proyecto.

---
+++ 3P Actividad Diccionarios de Objetos y Herencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3P_Actividad_Diccionarios_de_Objetos_y_Herencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
